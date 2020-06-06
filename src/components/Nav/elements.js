8ca1e216a3db889eb31b2b9e255456af01dc49ff
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-06 15:30:41 -0700

    add active icons to nav elements data

--- src/components/Nav/elements.js
+++ src/components/Nav/elements.js
@@ -1,23 +1,31 @@
 import homeIcon from '../../assets/icons/home.png';
+import homeActiveIcon from '../../assets/icons/home-active.png';
 import aboutIcon from '../../assets/icons/about.png';
-import workIcon from '../../assets/icons/work.png';
+import aboutActiveIcon from '../../assets/icons/about-active.png';
+import projectsIcon from '../../assets/icons/projects.png';
+import projectsActiveIcon from '../../assets/icons/projects-active.png';
 import contactIcon from '../../assets/icons/contact.png';
+import contactActiveIcon from '../../assets/icons/contact-active.png';
 
 export const elements = [
   {
     to: 'Home',
     icon: homeIcon,
+    activeIcon: homeActiveIcon
   },
   {
     to: 'About',
     icon: aboutIcon,
+    activeIcon: aboutActiveIcon
   },
   {
-    to: 'Work',
-    icon: workIcon,
+    to: 'Projects',
+    icon: projectsIcon,
+    activeIcon: projectsActiveIcon
   },
   {
     to: 'Contact',
     icon: contactIcon,
+    activeIcon: contactActiveIcon
   },
 ];
