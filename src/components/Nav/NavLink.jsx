7903dc9e93e17cab981476fbce3d525a2a9ecd2b
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-06 15:31:41 -0700

    display solid icon when element is active

--- src/components/Nav/NavLink.jsx
+++ src/components/Nav/NavLink.jsx
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 import styles from './NavLink.css';
 import typeStyle from '../../styles/type.css';
 
-const NavLink = ({ to, icon }) => (
+const NavLink = ({ to, icon, activeIcon }) => (
   <Link
     to={to}
     spy={true}
     smooth={true}
     duration={400}
+    activeClass={styles.active}
     className={styles.NavLink}>
-    <img src={icon} alt={to} />
+    <img src={icon} alt={to} className={styles.outline} />
+    <img src={activeIcon} alt={`${to} active`} className={styles.solid} />
     <p className={typeStyle.allCaps}>{to}</p>
   </Link>
 );
@@ -19,6 +21,7 @@ const NavLink = ({ to, icon }) => (
 NavLink.propTypes = {
   to: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
+  activeIcon: PropTypes.string.isRequired,
 };
 
 export default NavLink;
