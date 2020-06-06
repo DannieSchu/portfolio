895177729063606f01409df9d8c975e2030c9361
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-06 15:32:25 -0700

    test active functionality with Projects component scaffolding

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from '../Home/Home';
 import NavBar from '../Nav/NavBar';
+import Projects from '../Projects/Projects';
 
 export default function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <Projects />
     </>
   );
 }
