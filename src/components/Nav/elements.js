b185dc9b49027c3b723efba3dd7464a6d4fa4d10
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-06 15:04:51 -0700

    create array of nav elements

---
+++ src/components/Nav/elements.js
@@ -0,0 +1,23 @@
+import homeIcon from '../../assets/icons/home.png';
+import aboutIcon from '../../assets/icons/about.png';
+import workIcon from '../../assets/icons/work.png';
+import contactIcon from '../../assets/icons/contact.png';
+
+export const elements = [
+  {
+    to: 'Home',
+    icon: homeIcon,
+  },
+  {
+    to: 'About',
+    icon: aboutIcon,
+  },
+  {
+    to: 'Work',
+    icon: workIcon,
+  },
+  {
+    to: 'Contact',
+    icon: contactIcon,
+  },
+];
