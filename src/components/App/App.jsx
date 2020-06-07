79ab62c1fcc74092bff7c4b83e1452cd554e2849
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-06 22:31:19 -0700

    create About component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Home from '../Home/Home';
 import NavBar from '../Nav/NavBar';
 import Projects from '../Projects/Projects';
+import AboutContainer from '../About/AboutContainer';
 
 export default function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <AboutContainer />
       <Projects />
     </>
   );
