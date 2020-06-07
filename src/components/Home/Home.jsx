c0e7ea3146bfe196d65fbd4827b0121991a585a4
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-06 22:36:06 -0700

    create snapshot test for About component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,15 +4,12 @@ import smallFlowers from '../../assets/images/small-flowers.png';
 import typeStyle from '../../styles/type.css';
 import styles from './Home.css';
 
-const Home = () => {
-  return (
-    <section id="Home" className={styles.Home}>
-      <h1 className={typeStyle.mainHeading}>Dannie<br />Schumaker</h1>
-      <img className={styles.largeFlowers} src={largeFlowers} alt='flower illustration' />
-      <img className={styles.smallFlowers} src={smallFlowers} alt='flower illustration' />
-    </section>
-  );
-};
+const Home = () => (
+  <section id="Home" className={styles.Home}>
+    <h1 className={typeStyle.mainHeading}>Dannie<br />Schumaker</h1>
+    <img className={styles.largeFlowers} src={largeFlowers} alt='flower illustration' />
+    <img className={styles.smallFlowers} src={smallFlowers} alt='flower illustration' />
+  </section>
+);
 
 export default Home;
-
