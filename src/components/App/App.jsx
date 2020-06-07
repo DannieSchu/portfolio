97e138c032a157a08a80259546a6c67cdfeec21b
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-07 00:40:10 -0700

    create Strengths component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from '../Home/Home';
 import NavBar from '../Nav/NavBar';
 import Projects from '../Projects/Projects';
+import Strengths from '../About/Strengths';
 import AboutContainer from '../About/AboutContainer';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <NavBar />
       <Home />
       <AboutContainer />
+      <Strengths />
       <Projects />
     </>
   );
