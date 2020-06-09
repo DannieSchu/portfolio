bf2e84e177df7b160daa9beda377bbddebfe5594
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-09 14:27:48 -0700

    create array of technologies

---
+++ src/components/About/techArr.js
@@ -0,0 +1,22 @@
+export const techArr = [
+  {
+    title: 'Languages',
+    definitions: ['JavaScript', 'HTML', 'CSS']
+  },
+  {
+    title: 'Libraries & Frameworks',
+    definitions: ['React', 'Redux', 'Node', 'Express']
+  },
+  {
+    title: 'Databases',
+    definitions: ['MongoDB (with Mongoose ODM)', 'PostresSQL']
+  },
+  {
+    title: 'Testing Suites',
+    definitions: ['Jest', 'Enzyme', 'QUnit']
+  },
+  {
+    title: 'Dev Tools',
+    definitions: ['Travis CI', 'Postman', 'Compass']
+  }
+];
