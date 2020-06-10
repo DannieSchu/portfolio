17166349c816e6e12d925eeeb2c76f98272f8e70
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-10 02:11:48 -0700

    relocatescroll navigation logic to custom hook

--- src/components/Nav/Arrow.jsx
+++ src/components/Nav/Arrow.jsx
@@ -1,45 +1,14 @@
 import React, { useState } from 'react';
-import { Events, scroller } from 'react-scroll';
+// import { Events, scroller } from 'react-scroll';
 import PropTypes from 'prop-types';
 import defaultArrow from '../../assets/icons/arrow.png';
 import defaultHoverArrow from '../../assets/icons/white-arrow-shadow.png';
 import styles from './Arrow.css';
+import { useGetScroll } from '../../hooks/getScroll';
 
 const Arrow = ({ element, container, icon, shadowIcon }) => {
   const [currIcon, setCurrIcon] = useState(icon || defaultArrow);
-
-  const scrollTo = element => {
-    scroller.scrollTo(element, {
-      duration: 800,
-      delay: 0,
-      smooth: 'easeInOutQuart'
-    });
-  };
-
-  const scrollToWithContainer = (element, container) => {
-
-    let goToContainer = new Promise(resolve => {
-
-      Events.scrollEvent.register('end', () => {
-        resolve();
-        Events.scrollEvent.remove('end');
-      });
-
-      scroller.scrollTo(container, {
-        duration: 800,
-        delay: 0,
-        smooth: 'easeInOutQuart'
-      });
-    });
-
-    goToContainer.then(() =>
-      scroller.scrollTo(element, {
-        duration: 800,
-        delay: 0,
-        smooth: 'easeInOutQuart',
-        containerId: container
-      }));
-  };
+  const { scrollTo, scrollToWithContainer } = useGetScroll();
 
   return <img 
     src={currIcon} 
