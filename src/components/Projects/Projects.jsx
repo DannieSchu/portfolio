e6d5e21950ae0f483ec6c7e547ecdf336c63252f
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-11 19:01:15 -0700

    render project carousel in projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Project from './Project';
+import Arrow from '../Nav/Arrow';
 import styles from './Projects.css';
+import typeStyle from '../../styles/type.css';
+import { projectsArr } from './projectsArr';
 
 const Projects = () => {
+  const projectElements = projectsArr.map(project => <Project key={project.title} {...project} />);
+
   return (
     <section name="Projects" className={styles.Projects}>
-
+      <h2 className={typeStyle.pageHeading}>Projects</h2>
+      <section className={styles.wrapper} >
+        {projectElements}
+      </section>
+      <div className={styles.arrow}>
+        <Arrow element="Contact" />
+      </div>
     </section>
   );
 };
