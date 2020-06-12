692bc276fb30f8b132afef8533db6383e02f9432
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-11 18:37:37 -0700

    create Project component

---
+++ src/components/Projects/Project.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Project.css';
+import typeStyles from '../../styles/type.css';
+import ProjectHover from './ProjectHover';
+
+const Project = ({ 
+  title, 
+  stack, 
+  year, 
+  description, 
+  githubFE, 
+  githubBE, 
+  site, 
+  image 
+}) => {
+  const [active, setActive] = useState(false);
+  const stackList = stack.map(techItem => `${techItem}`).join(' | ');
+
+  return (
+    <section className={styles.Project} onMouseOver={() => setActive(true)} onMouseLeave={() => setActive(false)}>
+      <img src={image} />
+      <article>
+        <div className={styles.heading}>
+          <a href={site} target="blank"><h3 className={typeStyles.subheading}>{title}</h3></a>
+          <h3 className={typeStyles.subheading}>{year}</h3>
+        </div>
+        <h4 className={typeStyles.allCaps}>{stackList}</h4>
+        {active && <ProjectHover description={description} githubFE={githubFE} githubBE={githubBE} site={site} />}
+      </article>
+    </section>
+  );
+};
+
+Project.propTypes = {
+  title: PropTypes.string.isRequired,
+  stack: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  year: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  githubFE: PropTypes.string,
+  githubBE: PropTypes.string,
+  site: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+export default Project;
