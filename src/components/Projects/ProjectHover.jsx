712fb006da46b9df084aca72f0d76d0f6aa75e51
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-11 18:18:34 -0700

    create component that to be rendered on hover

---
+++ src/components/Projects/ProjectHover.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import typeStyles from '../../styles/type.css';
+import styles from './ProjectHover.css';
+
+const ProjectHover = ({ description, githubBE, githubFE, site }) => (
+  <div className={styles.ProjectHover}>
+    <p className={typeStyles.body}>{description}</p>
+    <div className={githubFE && githubBE ? styles.spaceBetween : styles.centered}>
+      {githubFE && <a href={githubFE} target="blank" className={typeStyles.body}>Front-End Repo</a>}
+      {githubBE && <a href={githubBE} target="blank" className={typeStyles.body}>Back-End Repo</a>}
+    </div>
+    {site && <div className={styles.centered}>
+      <a href={site} target="blank" className={`${styles.site} ${typeStyles.body}`}>Visit the Site</a>
+    </div>}
+  </div>
+);
+
+ProjectHover.propTypes = {
+  description: PropTypes.string.isRequired,
+  githubFE: PropTypes.string,
+  githubBE: PropTypes.string,
+  site: PropTypes.string.isRequired,
+};
+
+export default ProjectHover;
