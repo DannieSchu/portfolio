9da6c7e6cd9b61665eac5a29d621ede1dcb69c3e
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-13 13:20:59 -0700

    add container to Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Project from './Project';
 import Arrow from '../Nav/Arrow';
+import useScrollBox from '../../hooks/getScrollBox';
+import { projectsArr } from '../../utils/projectsArr';
 import styles from './Projects.css';
 import typeStyle from '../../styles/type.css';
-import { projectsArr } from '../../utils/projectsArr';
 
 const Projects = () => {
   const projectElements = projectsArr.map(project => <Project key={project.title} {...project} />);
 
+  const scrollWrapperRef = useRef();
+  const { isDragging, active } = useScrollBox(scrollWrapperRef);
+
   return (
     <section name="Projects" className={styles.Projects}>
       <h2 className={typeStyle.pageHeading}>Projects</h2>
-      <section className={styles.wrapper} >
-        {projectElements}
+      <section className={styles.wrapper} ref={scrollWrapperRef}>
+        <section style={{ pointerEvents: isDragging ? 'none' : undefined, 
+          // cursor: active ? 'grab' : 'grabbing' 
+        }} className={styles.container}>
+          {projectElements}
+        </section>
       </section>
       <div className={styles.arrow}>
         <Arrow element="Contact" />
