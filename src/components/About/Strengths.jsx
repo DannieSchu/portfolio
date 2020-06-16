31b7e02bc77f7fcc2685d717bd1785f1b93c70b2
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-16 11:00:29 -0700

    improve inkblot image quality

--- src/components/About/Strengths.jsx
+++ src/components/About/Strengths.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Arrow from '../Nav/Arrow';
-import inkBlot from '../../assets/images/splotch.png';
-import roundInkBlot from '../../assets/images/round-splotch.png';
+import inkBlot1 from '../../assets/images/splotch1.png';
+import inkBlot2 from '../../assets/images/splotch2.png';
+import inkBlot3 from '../../assets/images/splotch3.png';
 import typeStyle from '../../styles/type.css';
 import styles from './Strengths.css';
   
@@ -10,7 +11,7 @@ const Strengths = () => {
     <section name='Strengths' className={styles.Strengths}>
       <h2 className={typeStyle.pageHeading}>Strengths</h2>
       <div className={`${styles.element} ${styles.learner}`}>
-        <img src={roundInkBlot} alt="Ink Blot" />
+        <img src={inkBlot3} alt="Ink Blot" />
         <div className={styles.content}>
           <h3 className={typeStyle.subheading}>Lifelong Learner</h3>
           <ul className={`${styles.details} ${typeStyle.body}`}>
@@ -21,14 +22,14 @@ const Strengths = () => {
         </div>
       </div>
       <div className={`${styles.element} ${styles.quality}`}>
-        <img src={inkBlot} alt="Ink Blot" />
+        <img src={inkBlot2} alt="Ink Blot" />
         <div className={styles.content}>
           <h3 className={typeStyle.subheading}>Ethos for Quality</h3>
           <p className={`${styles.details} ${typeStyle.body}`}>"Dannie has an <i>extreme</i> amount of attention to detail"<br /><i>-a senior developer</i></p>
         </div>
       </div>
       <div className={`${styles.element} ${styles.problemSolver}`}>
-        <img src={inkBlot} alt="Ink Blot" />
+        <img src={inkBlot1} alt="Ink Blot" />
         <div className={styles.content}>
           <h3 className={typeStyle.subheading}>Creative Problem Solver</h3>
           <p className={`${styles.details} ${typeStyle.body}`}>Some stuff here...fneonwoegwep jopjv<br /> fiewohgwe0fhiosbhsoens</p>
