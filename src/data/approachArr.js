8b4548d5ccb626bc705fe4dad481810623109507
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-26 13:41:22 -0700

    add approach data

---
+++ src/data/approachArr.js
@@ -0,0 +1,39 @@
+import inkBlot1 from '../assets/images/splotch1.png';
+import inkBlot2 from '../assets/images/splotch2.png';
+import inkBlot3 from '../assets/images/splotch3.png';
+
+export const approachArr = [
+  {
+    image: inkBlot3,
+    name: 'design',
+    heading: 'Design Focus',
+    paragraph: [
+      {
+        body: 'Creative professional background and robust nonprofit experience inspire dedication to making lives easier.'
+      }, {
+        body: 'Strives to make apps easy to navigate for both end users and other developers through intuitive design and engineering practices.'
+      }]
+  },
+  {
+    image: inkBlot2,
+    name: 'learning',
+    heading: 'Endless Learning',
+    paragraph: [{
+      body: 'Dives deep into unfamiliar technologies and practices to realize a vision.'
+    }, {
+      link: 'http://www.drsscientificart.com/archaeological-facial-reconstruction/',
+      linkText: 'View example ',
+      body: 'of technology implementation from illustraton career.'
+    }]
+  },
+  {
+    image: inkBlot1,
+    name: 'process',
+    heading: 'Passion for Process',
+    paragraph: [{
+      body: 'Rigorous at each step of software development process to reduce technical debt.'
+    }, {
+      body: 'Breaks down processes with an attention to quality to build elegant applications with clean, reusable, maintainable code.'
+    }],
+  },
+];
