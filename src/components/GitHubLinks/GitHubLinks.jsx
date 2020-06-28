6cd62d7906e81ca23171fada00ee027abb65fc10
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-28 15:45:29 -0700

    create reusable component for github links

---
+++ src/components/GitHubLinks/GitHubLinks.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './GitHubLinks.css';
+import typeStyles from '../../styles/type.css';
+
+const GitHubLinks = ({ githubFE, githubBE }) => (
+  <div className={githubFE && githubBE ? styles.spaceBetween : styles.centered}>
+    {githubFE && <a href={githubFE} target="blank" className={typeStyles.body}>Front-End Repo</a>}
+    {githubBE && <a href={githubBE} target="blank" className={typeStyles.body}>Back-End Repo</a>}
+  </div>
+);
+
+GitHubLinks.propTypes = {
+  githubBE: PropTypes.string,
+  githubFE: PropTypes.string
+};
+
+export default GitHubLinks;
