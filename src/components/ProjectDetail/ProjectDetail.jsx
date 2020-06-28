8207ebd30cbf18d5d15fcf7271ecc4111084eefd
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-28 16:27:16 -0700

    use LargeButton in Contact and ProjectDetail

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Carousel from '../Carousel/Carousel';
 import TextBlock from '../TextBlock/TextBlock';
 import StackList from '../StackList/StackList';
+import GitHubLinks from '../GithubLinks/GitHubLinks';
 import { projectsArr } from '../../data/projectsArr';
 import styles from './ProjectDetail.css';
 import typeStyle from '../../styles/type.css';
-import GitHubLinks from '../GithubLinks/GitHubLinks';
+import LargeButton from '../LargeButton/LargeButton';
 
 const ProjectDetail = () => {
   const [currentProject, setCurrentProject] = useState({});
@@ -15,28 +16,33 @@ const ProjectDetail = () => {
 
   useEffect(() => {
     const foundProject = projectsArr.find(project => project.title === title);
-    
+
     setCurrentProject(foundProject);
     setCurrentImages(foundProject.images);
     setCurrentStack(foundProject.stack);
   }, []);
 
   return (
-    <section>
-      <section className={styles.ProjectDetail}>
-        <section className={styles.container}>
-          <section className={styles.column}>
-            <h2 className={`${styles.heading} ${typeStyle.pageHeading}`}>{title}</h2>
+    <section className={styles.ProjectDetail}>
+      <section className={styles.container}>
+        <section className={styles.column}>
+          <section className={styles.heading}>
+            <h2 className={typeStyle.pageHeading}>{title}</h2>
             <StackList stack={currentStack} />
+          </section>
+          <section className={styles.textOverview}>
             <TextBlock heading="Overview" content={currentProject.overview} />
             <TextBlock heading="Process" content={currentProject.process} />
+            <aside className={styles.website}>
+              <LargeButton link={currentProject.website}>View Site</LargeButton>
+            </aside>
           </section>
         </section>
-        <section className={styles.container}>
-          <section className={styles.column}>
-            <Carousel images={currentImages} />
-            <GitHubLinks githubBE={currentProject.githubBE} githubFE={currentProject.githubFE} />
-          </section>
+      </section>
+      <section className={styles.container}>
+        <section className={styles.column}>
+          <Carousel images={currentImages} />
+          <GitHubLinks githubBE={currentProject.githubBE} githubFE={currentProject.githubFE} />
         </section>
       </section>
     </section>
