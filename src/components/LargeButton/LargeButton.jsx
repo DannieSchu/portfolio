806387f9afe5eb2d6695a86dbc3cdfee462e3250
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-01 15:53:13 -0700

    improve component readability

--- src/components/LargeButton/LargeButton.jsx
+++ src/components/LargeButton/LargeButton.jsx
@@ -3,7 +3,16 @@ import PropTypes from 'prop-types';
 import styles from './LargeButton.css';
 import typeStyle from '../../styles/type.css';
 
-const LargeButton = ({ link, children }) => <a href={link} target="_blank" rel="noopener noreferrer" className={`${styles.LargeButton} ${typeStyle.body}`}>{children}</a>;
+const LargeButton = ({ link, children }) => (
+  <a
+    href={link}
+    target="_blank"
+    rel="noopener noreferrer"
+    className={`${styles.LargeButton} ${typeStyle.body}`}
+  >
+    {children}
+  </a>
+);
 
 LargeButton.propTypes = {
   link: PropTypes.string.isRequired,
--- src/components/PageHeading/PageHeading.jsx
+++ src/components/PageHeading/PageHeading.jsx
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types';
 import typeStyle from '../../styles/type.css';
 import styles from './PageHeading.css';
 
-const PageHeading = ({ content }) => {
-  return (
-    <div className={styles.PageHeading}>
-      <h2 className={typeStyle.pageHeading}>{content}</h2>
-    </div>
-  );
-};
+const PageHeading = ({ content }) => (
+  <div className={styles.PageHeading}>
+    <h2 className={typeStyle.pageHeading}>{content}</h2>
+  </div>
+);
 
 PageHeading.propTypes = {
   content: PropTypes.string.isRequired
