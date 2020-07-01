c32cc893f3b22905a5e05bbe8f317bd103bd0fb7
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-30 17:36:49 -0700

    add contributors and images to data

--- src/data/projects/alcherithm.js
+++ src/data/projects/alcherithm.js
@@ -5,3 +5,22 @@ import alcherithmResults from '../../assets/projects/alcherithm/results-wide.png
 import alcherithmHistory from '../../assets/projects/alcherithm/history.png';
 
 export const alcherithmImages = [alcherithmDashboard, alcherithmList, alcherithmTest, alcherithmResults, alcherithmHistory];
+
+export const alcherithmContributors = [
+  {
+    name: 'Dakota Druley',
+    link: 'https://github.com/dakotadruley'
+  },
+  {
+    name: 'Janelle Mellor',
+    link: 'https://github.com/janellemellor'
+  },
+  {
+    name: 'Will Piro',
+    link: 'https://github.com/Shaka2Pass'
+  },
+  {
+    name: 'Mikey Romay',
+    link: 'https://github.com/mikeymasonic'
+  }
+];
---
+++ src/data/projects/socialDistancePing.js
@@ -0,0 +1,24 @@
+import socialDistancePingPhoto from '../../assets/projects/social-distance-ping/photo.jpeg';
+import socialDistancePingDatabase from '../../assets/projects/social-distance-ping/mlab.png';
+import socialDistancePingPostman from '../../assets/projects/social-distance-ping/postman.png';
+
+export const socialDistancePingImages = [socialDistancePingPhoto, socialDistancePingDatabase, socialDistancePingPostman];
+
+export const socialDistancePingContributors = [
+  {
+    name: 'Jenna Goldman',
+    link: 'https://github.com/jennagoldman'
+  },
+  {
+    name: 'Rosalie Lee',
+    link: 'https://github.com/rosalie337'
+  },
+  {
+    name: 'Hannah Riley',
+    link: 'https://github.com/hannahriley33'
+  },
+  {
+    name: 'Chelsea Spangler',
+    link: 'https://github.com/CNSpangler'
+  }
+];
