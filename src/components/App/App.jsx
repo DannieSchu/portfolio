1cfd53c0b7440731fc06c673e684cc2b23917cfc
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-03 01:16:28 -0700

    display spinner while app loads

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../Home/Home';
 import NavBar from '../Nav/NavBar';
@@ -6,8 +6,21 @@ import Projects from '../Projects/Projects';
 import AboutContainer from '../About/AboutContainer';
 import Contact from '../Contact/Contact';
 import ProjectDetail from '../ProjectDetail/ProjectDetail';
+import Loading from '../Loading/Loading';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+  
+  if(isLoading) {
+    return <Loading />;
+  }
+  
   return (
     <Router>
       <Switch>
