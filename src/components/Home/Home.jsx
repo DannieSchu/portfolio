4d431c73265d1ec69b45d2d80ab3660c9cd8c67e
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-03 02:02:36 -0700

    fade in Home and NavBar components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Arrow from '../Nav/Arrow';
 import largeFlowers from '../../assets/images/large-flowers.png';
 import smallFlowers from '../../assets/images/small-flowers.png';
@@ -7,13 +7,21 @@ import salmonShadowIcon from '../../assets/icons/salmon-arrow-shadow.png';
 import typeStyle from '../../styles/type.css';
 import styles from './Home.css';
 
-const Home = () => (
-  <section name="Home" className={styles.Home}>
-    <h1 className={typeStyle.mainHeading}>Dannie<br />Schumaker</h1>
-    <img className={styles.largeFlowers} src={largeFlowers} alt='flower illustration' />
-    <img className={styles.smallFlowers} src={smallFlowers} alt='flower illustration' />
-    <Arrow element="About" container="AboutContainer" icon={salmonIcon} shadowIcon={salmonShadowIcon} />
-  </section>
-);
+const Home = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  const opacity = isMounted ? 1 : 0;
+
+  return (
+    <section name="Home" className={styles.Home} style={{ opacity }}>
+      <h1 className={typeStyle.mainHeading}>Dannie<br />Schumaker</h1>
+      <img className={styles.largeFlowers} src={largeFlowers} alt='flower illustration' />
+      <img className={styles.smallFlowers} src={smallFlowers} alt='flower illustration' />
+      <Arrow element="About" container="AboutContainer" icon={salmonIcon} shadowIcon={salmonShadowIcon} />
+    </section>
+  );
+};
 
 export default Home;
--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavLink from './NavLink';
 import { navIconsData } from '../../data/navIconsData';
 import styles from './NavBar.css';
 
 const NavBar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  const opacity = isMounted ? 1 : 0;
+
   const navElements = navIconsData.map((element, i) => <NavLink key={i} {...element} />);
 
   return (
-    <nav className={styles.NavBar}>
+    <nav className={styles.NavBar} style={{ opacity }}>
       {navElements}
     </nav>
   );
