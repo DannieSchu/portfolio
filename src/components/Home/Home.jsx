61d9368a2ea0218a2695604743725ee669109be4
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-03 02:50:22 -0700

    display homepage after image load

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Arrow from '../Nav/Arrow';
 import largeFlowers from '../../assets/images/large-flowers.png';
 import smallFlowers from '../../assets/images/small-flowers.png';
 import salmonIcon from '../../assets/icons/salmon-arrow.png';
 import salmonShadowIcon from '../../assets/icons/salmon-arrow-shadow.png';
-import { useGetOpacity } from '../../hooks/getOpacity';
 import typeStyle from '../../styles/type.css';
 import styles from './Home.css';
 
 const Home = () => {
-  const opacity = useGetOpacity();
+  const [imageOneLoaded, setImageOneLoaded] = useState(false);
+  const [imageTwoLoaded, setImageTwoLoaded] = useState(false);
+
+  const opacity = (imageOneLoaded && imageTwoLoaded) ? 1 : 0;
 
   return (
     <section name="Home" className={styles.Home} style={{ opacity }}>
       <h1 className={typeStyle.mainHeading}>Dannie<br />Schumaker</h1>
-      <img className={styles.largeFlowers} src={largeFlowers} alt='flower illustration' />
-      <img className={styles.smallFlowers} src={smallFlowers} alt='flower illustration' />
+      <img className={styles.largeFlowers} src={largeFlowers} onLoad={() => setImageOneLoaded(true)} alt='flower illustration' />
+      <img className={styles.smallFlowers} src={smallFlowers} onLoad={() => setImageTwoLoaded(true)} alt='flower illustration' />
       <Arrow element="About" container="AboutContainer" icon={salmonIcon} shadowIcon={salmonShadowIcon} />
     </section>
   );
