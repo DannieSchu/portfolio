92bc0e319802589461740cc0c55b37f9775ed713
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-08 13:59:58 -0700

    upload images for Safe Place app

---
+++ src/data/projects/safePlaceForYouth.js
@@ -0,0 +1,20 @@
+import home from '../../assets/projects/safe-place/home.jpeg';
+import libraryDetail from '../../assets/projects/safe-place/library-detail.jpeg';
+import map from '../../assets/projects/safe-place/map.jpeg';
+import fireStationMap from '../../assets/projects/safe-place/fire-station-map.jpeg';
+import liasonsList from '../../assets/projects/safe-place/liasons-list.jpeg';
+import fireStationsList from '../../assets/projects/safe-place/fire-stations-list.jpeg';
+import fireStationDetail from '../../assets/projects/safe-place/fire-station-detail.jpeg';
+
+export const safePlaceForYouthImages = [home, libraryDetail, map, fireStationMap, liasonsList, fireStationsList, fireStationDetail];
+
+export const safePlaceForYouthContributors = [
+  {
+    name: 'Jenna Goldman',
+    link: 'https://github.com/jennagoldman'
+  },
+  {
+    name: 'Chelsea Spangler',
+    link: 'https://github.com/CNSpangler'
+  }
+];
