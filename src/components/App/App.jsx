12803b23b4041b0e5f3a99527581039587993b66
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-12 15:37:04 -0700

    render Home and Nav without opacity state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,5 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Home from '../Home/Home';
-// import NavBar from '../Nav/NavBar';
 import Projects from '../Projects/Projects';
 import AboutContainer from '../About/AboutContainer';
 import Contact from '../Contact/Contact';
--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import NavLink from './NavLink';
-import { useOpacity } from '../../hooks/LoadingProvider';
+// import { useOpacity } from '../../hooks/LoadingProvider';
 import { navIconsData } from '../../data/navIconsData';
 import styles from './NavBar.css';
 
 const NavBar = () => {
-  const opacity = useOpacity();
-
+  // const opacity = useOpacity();
   const navElements = navIconsData.map((element, i) => <NavLink key={i} {...element} />);
 
   return (
-    <nav className={styles.NavBar} style={{ opacity }}>
+    <nav className={styles.NavBar}>
       {navElements}
     </nav>
   );
