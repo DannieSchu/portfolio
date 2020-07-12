c1411465be19bb2348d760ad717e43f558e99d7b
Dannie Schumaker <draeschumaker@gmail.com>
2020-07-12 15:04:59 -0700

    implement code splitting in router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../Home/Home';
-import NavBar from '../Nav/NavBar';
+// import Home from '../Home/Home';
+// import NavBar from '../Nav/NavBar';
 import Projects from '../Projects/Projects';
 import AboutContainer from '../About/AboutContainer';
 import Contact from '../Contact/Contact';
-import ProjectDetail from '../ProjectDetail/ProjectDetail';
 import Loading from '../Loading/Loading';
-import { useLoading, useTimeoutStatus } from '../../hooks/LoadingProvider';
+const Home = lazy(() => import('../Home/Home'));
+const NavBar = lazy(() => import('../Nav/NavBar'));
+const ProjectDetail = lazy(() => import('../ProjectDetail/ProjectDetail'));
 
 export default function App() {
-  const isLoading = useLoading();
-  const timeoutStatus = useTimeoutStatus();
-
-  if(isLoading) {
-    return <Loading timeoutStatus={timeoutStatus} background="fullscreen" />;
-  }
-
   return (
     <Router>
-      <Switch>
-        <Route exact path='/project/:title' component={ProjectDetail} />
-        <Route exact path='/'>
-          <NavBar />
-          <Home />
-          <AboutContainer />
-          <Projects />
-          <Contact />
-        </Route>
-      </Switch>
+      <Suspense fallback={<Loading background="fullscreen" />}>
+        <Switch>
+          <Route exact path='/project/:title' component={ProjectDetail} />
+          <Route exact path='/'>
+            <NavBar />
+            <Home />
+            <AboutContainer />
+            <Projects />
+            <Contact />
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
